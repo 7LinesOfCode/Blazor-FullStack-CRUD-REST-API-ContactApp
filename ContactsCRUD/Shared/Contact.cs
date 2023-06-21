2af2ca02257828155576f66c470294448e0748eb
7LinesOfCode <jjjiw7ct@gmail.com>
2023-06-21 03:41:58 +0200

    Completed

--- ContactsCRUD/Shared/Contact.cs
+++ ContactsCRUD/Shared/Contact.cs
@@ -12,6 +12,7 @@ namespace ContactsCRUD.Shared
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+
         public string PhoneNumber { get; set; }
         public Category? Category { get; set; }
         public string Note { get; set; } = string.Empty;
