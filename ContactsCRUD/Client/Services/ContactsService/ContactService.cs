22d453d329c92bbd44df86fcefd8d12c06867973
7LinesOfCode <jjjiw7ct@gmail.com>
2023-06-21 03:16:41 +0200

    Done

--- ContactsCRUD/Client/Services/ContactsService/ContactService.cs
+++ ContactsCRUD/Client/Services/ContactsService/ContactService.cs
@@ -1,4 +1,5 @@
 ﻿using ContactsCRUD.Shared;
+using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 
 
@@ -7,10 +8,12 @@ namespace ContactsCRUD.Client.Services.ContactsService
     public class ContactService : IContactService
     {
         private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
 
-        public ContactService(HttpClient http)
+        public ContactService(HttpClient http, NavigationManager navigationManager)
         {
             _http= http;
+            _navigationManager = navigationManager;
         }
 
         public List<Contact> Contacts { get; set; } = new List<Contact>();
@@ -19,15 +22,21 @@ namespace ContactsCRUD.Client.Services.ContactsService
         public async Task CreateContact(Contact contact)
         {
             var result = await _http.PostAsJsonAsync("api/contact", contact);
+            await SetContact(result);
+        }
+
+        private async Task SetContact(HttpResponseMessage result)
+        {
             var response = await result.Content.ReadFromJsonAsync<List<Contact>>();
             Contacts = response;
+            _navigationManager.NavigateTo("contacts");
         }
 
         public async Task DeleteContact(int id)
         {
             var result = await _http.DeleteAsync($"api/contact/{id}");
             var response = await result.Content.ReadFromJsonAsync<List<Contact>>();
-            Contacts = response;
+            await SetContact(result);
         }
 
         public async Task GetCategories()
@@ -64,8 +73,7 @@ namespace ContactsCRUD.Client.Services.ContactsService
         public async Task UpdateContact(Contact contact)
         {
             var result = await _http.PutAsJsonAsync($"api/contact/{contact.Id}", contact);
-            var response = await result.Content.ReadFromJsonAsync<List<Contact>>();
-            Contacts = response;
+            await SetContact(result);
         }
     }
 }
